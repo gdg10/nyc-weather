7f00e34d9b360f5ed887facec982836c7db8d22c
Garrett Grube <ggrube@quartethealth.com>
2019-11-18 17:17:31 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import logo from "./statue-of-liberty-icon.jpg";
 import "./App.css";
 import CurrentWeather from "./CurrentWeather";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Spinner} from "react-bootstrap";
 
 class App extends Component {
   state = {
-    currentWeather: null
+    currentWeather: null,
   };
 
   componentDidMount() {
@@ -25,9 +26,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           {this.state.currentWeather === null ? (
-            <p></p>
+            <Spinner animation="border" variant="light" />
           ) : (
             <CurrentWeather currentWeather={this.state.currentWeather} />
           )}
--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,17 +1,20 @@
 import React from "react";
-//import styled from "styled-components";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import logo from "./statue-of-liberty-icon.jpg";
 
 const CurrentWeather = ({ currentWeather }) => {
   return (
     <Container>
       <Row>
-        <Col>{currentWeather.weather[0].main}</Col>
-        <Col>{currentWeather.main.temp}°</Col>
-      </Row>
-      <Row>
-        <Col>foo</Col>
-        <Col>{currentWeather.main.temp_max}° / {currentWeather.main.temp_min}°</Col>
+        <Col md="auto">
+          <Image src={logo} className="App-logo" alt="logo" />
+        </Col>
+        <Col md="auto">
+          <h2>{currentWeather.weather[0].main}</h2><h4>({currentWeather.weather[0].description})</h4>
+        </Col>
+        <Col md="auto">
+          <h2>{currentWeather.main.temp}°</h2><h4>{currentWeather.main.temp_min}° / {currentWeather.main.temp_max}°</h4>
+        </Col>
       </Row>
     </Container>
   );
