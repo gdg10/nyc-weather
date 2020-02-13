dc7dec4ad5a6237b44d2b9ca801a800a00e8336b
Garrett Grube <ggrube@quartethealth.com>
2020-02-13 15:20:36 -0500

    size edit

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -93,7 +93,7 @@ const CurrentWeather = ({ weather }) => {
               title="calendar"
               src="https://calendar.google.com/calendar/embed?height=200&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=Z2dydWJlQHF1YXJ0ZXRoZWFsdGguY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;showTz=0&amp;showCalendars=0&amp;mode=AGENDA&amp;showNav=0&amp;showDate=0&amp;showTitle=0&amp;showPrint=0&amp;showTabs=0"
               style={{float: 'left', marginTop: '2em', opacity: '.85', borderRadius: '3px'}}
-              width="300"
+              width="355"
               height="200"
               frameborder="0"
               scrolling="no"
